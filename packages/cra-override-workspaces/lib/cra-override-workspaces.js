27951da094bb085e853b8a0c85a70b283a05285a
fupeng <fupenglxy@gmail.com>
2022-05-13 14:14:48 +0800

    fix(cra-override-workspaces): support esbuild

--- packages/cra-override-workspaces/lib/cra-override-workspaces.js
+++ packages/cra-override-workspaces/lib/cra-override-workspaces.js
@@ -1,23 +1,36 @@
 const { monorepoDependenciesLocalPaths } = require('monorepo-dependencies');
-const {
-  override,
-  getBabelLoader,
-  removeModuleScopePlugin,
-} = require('customize-cra');
+const { override, getBabelLoader, removeModuleScopePlugin } = require('customize-cra');
 
 module.exports = craOverrideWorkspaces;
 
 function craOverrideWorkspaces(pkgName = process.env.npm_package_name) {
   process.env.SKIP_PREFLIGHT_CHECK = true;
 
-  return override(config => {
+  return override((config) => {
     const babelLoad = getBabelLoader(config);
-    babelLoad.include = [
-      ...(Array.isArray(babelLoad.include)
-        ? babelLoad.include
-        : [babelLoad.include]),
-      ...(monorepoDependenciesLocalPaths(pkgName) || []),
-    ];
+    if (babelLoad) {
+      babelLoad.include = [
+        ...(Array.isArray(babelLoad.include) ? babelLoad.include : [babelLoad.include]),
+        ...(monorepoDependenciesLocalPaths(pkgName) || []),
+      ];
+    }
+
+    // if you use esbuild-loader
+    const loaders = config.module.rules.find((rule) => Array.isArray(rule.oneOf)).oneOf;
+    if (loaders) {
+      const esbuildLoader = loaders.find((rule) => {
+        return rule.loader && rule.loader.includes('esbuild');
+      });
+      if (esbuildLoader) {
+        esbuildLoader.include = [
+          ...(Array.isArray(esbuildLoader.include)
+            ? esbuildLoader.include
+            : [esbuildLoader.include]),
+          ...(monorepoDependenciesLocalPaths(pkgName) || []),
+        ];
+      }
+    }
+
     return config;
   }, removeModuleScopePlugin());
 }
